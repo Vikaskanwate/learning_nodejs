7c6ed32163f9a7fa31f5cb55a98bf6c9e154d061
Vikaskanwate <vikaskanwate32@gmail.com>
2025-07-02 20:07:30 +0530

    practice

--- 03_node/index.js
+++ 03_node/index.js
@@ -1,43 +1,97 @@
 const express = require("express");
-const users = require('./MOCK_DATA.json');
+const mongoose = require('mongoose');
+
+
 const app = express();
-const fs = require('fs');
 const PORT = 3000;
 
-app.use(express.urlencoded({extended : false}));
+// connection
+mongoose
+        .connect('mongodb://localhost:27017/crud')
+        .then(()=>console.log("Mongo connected"))
+        .catch((err)=>console.log("mongo error",err));
 
-app.get('/api/users', (req, res) => {
-    res.json(users);
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type:String,
+        required:true,
+    },
+    lastName: {
+        type:String,
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true,
+    },
+    jobTitle:{
+        type:String
+    },
+    gender:{
+        type:String
+    }
+});
+
+const User = mongoose.model("user",userSchema);
+// this is middleware which is use to valiate and process the request before sending to the client or receiving from client 
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use((req, res, next) => {
+    console.log("hello from middleware")
+    next();
+})
+
+
+// REST API getAll Users
+app.get('/api/users', async(req, res) => {
+    const allDbUsers = await User.find({});
+    return res.json(allDbUsers);
 });
 
 
+// get User by id
 app
     .route("/api/users/:id")
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
+    .get(async(req, res) => {
+        const user = await User.findById(req.params.id);
+        if(!user) return res.status(404).json({error:"user not found"});
         return res.json(user);
     })
-    .patch((req,res)=>{
-        res.json({status:"pending"});
+    .patch( async (req, res) => {
+        await User.findByIdAndUpdate(req.params.id,{lastName:"changed"});
+        return res.json({ status: "success" });
     })
-    .delete((req,res)=>{
-
-            console.log(req.params.id)
-            delete users.id;
-            return res.json({status : "succes"})
-        }
-        return res.json({status : "faild"});
+    .delete(async(req, res) => {
+        await User.findByIdAndDelete(req.params.id)
+        return res.json({status:"success"});
     });
 
 
-app.post('/api/users',(req,res)=>{
+app.post('/api/users', async (req, res) => {
     const body = req.body;
-    users.push({...body, id: users.length + 1});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-        return res.json({status :"success",id : users.length});
-    });
-    console.log(body);
+    if(
+        !body ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title 
+    ){
+        return res.status(400).json({msg:"all fields are req.."});
+    }
+
+    const result = await User.create({
+        firstName:body.first_name,
+        lastName:body.last_name,
+        email:body.email,
+        gender:body.gender,
+        jobTitle:body.job_title,
+    })
+
+    console.log("result",result);
+    return res.status(201).json({msg:"success"});
+    
 })
 
 app.listen(PORT, () => console.log("application is started"));
