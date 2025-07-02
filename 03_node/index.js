f7c508b9f0ce5b7a20f2e594950ecdb4db3fe19d
Vikaskanwate <vikaskanwate32@gmail.com>
2025-07-03 00:22:18 +0530

    practice

--- 03_node/index.js
+++ 03_node/index.js
@@ -1,97 +1,19 @@
-const express = require("express");
-const mongoose = require('mongoose');
-
-
+const express = require('express');
+const userRouter = require('./routes/userRouter');
+const {connectMongoDb} = require('./connection');
+const {logReqRes} = require('./log'); 
 const app = express();
 const PORT = 3000;
 
-// connection
-mongoose
-        .connect('mongodb://localhost:27017/crud')
-        .then(()=>console.log("Mongo connected"))
-        .catch((err)=>console.log("mongo error",err));
+connectMongoDb('mongodb://localhost:27017/crud');
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type:String,
-        required:true,
-    },
-    lastName: {
-        type:String,
-    },
-    email: {
-        type:String,
-        required:true,
-        unique:true,
-    },
-    jobTitle:{
-        type:String
-    },
-    gender:{
-        type:String
-    }
-});
 
-const User = mongoose.model("user",userSchema);
+
 // this is middleware which is use to valiate and process the request before sending to the client or receiving from client 
 app.use(express.urlencoded({ extended: false }));
 
+app.use(logReqRes('log.txt'));
 
-app.use((req, res, next) => {
-    console.log("hello from middleware")
-    next();
-})
-
-
-// REST API getAll Users
-app.get('/api/users', async(req, res) => {
-    const allDbUsers = await User.find({});
-    return res.json(allDbUsers);
-});
-
-
-// get User by id
-app
-    .route("/api/users/:id")
-    .get(async(req, res) => {
-        const user = await User.findById(req.params.id);
-        if(!user) return res.status(404).json({error:"user not found"});
-        return res.json(user);
-    })
-    .patch( async (req, res) => {
-        await User.findByIdAndUpdate(req.params.id,{lastName:"changed"});
-        return res.json({ status: "success" });
-    })
-    .delete(async(req, res) => {
-        await User.findByIdAndDelete(req.params.id)
-        return res.json({status:"success"});
-    });
-
-
-app.post('/api/users', async (req, res) => {
-    const body = req.body;
-    if(
-        !body ||
-        !body.first_name ||
-        !body.last_name ||
-        !body.email ||
-        !body.gender ||
-        !body.job_title 
-    ){
-        return res.status(400).json({msg:"all fields are req.."});
-    }
-
-    const result = await User.create({
-        firstName:body.first_name,
-        lastName:body.last_name,
-        email:body.email,
-        gender:body.gender,
-        jobTitle:body.job_title,
-    })
-
-    console.log("result",result);
-    return res.status(201).json({msg:"success"});
-    
-})
+app.use('/user',userRouter)
 
 app.listen(PORT, () => console.log("application is started"));
---
+++ 03_node/routes/userRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {handleGetUserById, handleUpdateUserById, handleDeleteUserById, handleCreateNewUser, handleGetAllUsers} =  require('../controllers/user');
+
+// REST API getAll Users
+router.get("/",handleGetAllUsers).post(handleCreateNewUser);
+
+// get User by id
+router
+    .route("/:id")
+    .get(handleGetUserById)
+    .patch(handleUpdateUserById)
+    .delete(handleDeleteUserById);
+
+
+module.exports = router;
