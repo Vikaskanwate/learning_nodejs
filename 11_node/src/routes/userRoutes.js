425caee0b8742b7819a43fb8464ebfbe31d5ca7c
Vikaskanwate <vikaskanwate32@gmail.com>
2025-07-13 00:22:53 +0530

    practice

--- 11_node/src/routes/userRoutes.js
+++ 11_node/src/routes/userRoutes.js
@@ -1,24 +1,34 @@
 const express = require('express');
-
+const verifyToken = require('../middleware/authmiddleware');
 const router = express.Router();
+const authorizeRoles = require('../middleware/roleMiddleware');
+router.get(
+    "/admin",
+    verifyToken, authorizeRoles("admin"), (req, res) => {
+        res.json({
+            message: "welcome Admin"
+        });
+    })
 
-router.get("/admin",(req,res)=>{
-    res.json({
-        message:"welcome Admin"
-    });
-})
-
-router.get("/manager",(req,res)=>{
-    res.json({
-        message:"welcome manager"
-    });
-})
+router.get(
+    "/manager",
+    verifyToken,
+    authorizeRoles("manager", "admin"),
+    (req, res) => {
+        res.json({
+            message: "welcome manager"
+        });
+    })
 
 
-router.get("/user",(req,res)=>{
-    res.json({
-        message:"welcome user"
-    });
-})
+router.get(
+    "/user", 
+    verifyToken,
+    authorizeRoles("admin","user","manager"), 
+    (req, res) => {
+        res.json({
+            message: "welcome user"
+        });
+    })
 
 module.exports = router
