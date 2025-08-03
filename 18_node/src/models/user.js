2927999af12385abbb53df53e378e8a693fc92ce
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-03 15:53:34 +0530

    practice

---
+++ 18_node/src/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user"
+    }
+})
+
+userSchema.pre("save",async function() {
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password,8);
+    }
+    next();
+})
+
+userSchema.methods.comparepassword = async function () {
+    return await bcrypt.compare(password,this.password);
+}
+
+module.exports = mongoose.model("User",userSchema);
