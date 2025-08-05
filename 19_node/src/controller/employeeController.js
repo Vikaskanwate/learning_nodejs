c0d92bf6b493c56488def876da762465354caf29
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-05 22:42:20 +0530

    practice

--- 19_node/src/controller/employeeController.js
+++ 19_node/src/controller/employeeController.js
@@ -1,9 +1,9 @@
 const employeemodel = require('../models/employee');
-const createEmployee = async (req,res) =>{
-    const {name,designation,department,salary} = req.body;
-    try{
-        const emp = await employeemodel.findOne({name});
-        if(!emp){
+const createEmployee = async (req, res) => {
+    const { name, designation, department, salary } = req.body;
+    try {
+        const emp = await employeemodel.findOne({ name });
+        if (!emp) {
             const newEmp = new employeemodel({
                 name,
                 designation,
@@ -13,21 +13,41 @@ const createEmployee = async (req,res) =>{
 
             await newEmp.save();
             return res.status.json({
-                newEmployee:newEmp,
-                success:true
+                newEmployee: newEmp,
+                success: true
             })
         }
         return res.status(400).json({
-            msg:"Employee already exists"
+            msg: "Employee already exists"
         })
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return res.status(500).json({
-            msg:"Internal server error"
+            msg: "Internal server error"
+        })
+    }
+}
+
+const getAllEmployee = async (req, res) => {
+    try {
+        const emp = await employeemodel.find({});
+        if(!emp){
+            res.status(400).json({
+                msg:"Employees does not exists"
+            })
+        }
+        return res.status(200).json({
+            employees:emp
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            msg: "Internal server error"
         })
     }
 }
 
 module.exports = {
-    createEmployee
+    createEmployee,
+    getAllEmployee
 }
--- 19_node/src/routes/employeeRoutes.js
+++ 19_node/src/routes/employeeRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const {createEmployee} = require('../controller/employeeController');
+const {createEmployee,getAllEmployee} = require('../controller/employeeController');
 const router = express.Router();
 
 router.post("/",createEmployee);
+router.get("/",getAllEmployee);
 
 module.exports = router;
