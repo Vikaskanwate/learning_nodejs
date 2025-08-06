aea862b798891fa8dcc865ed70ca151096cd7fc2
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-06 23:47:12 +0530

    practice

--- 19_node/src/controller/userController.js
+++ 19_node/src/controller/userController.js
@@ -23,3 +23,31 @@ const register  = async (req,res) =>{
         })
     }
 }
+
+const login = async () =>{
+    const {username,password} = req.body;
+    try{
+        const user = await usermodel.findOne({username});
+        if(!user || !(await user.comparepassword(password))){
+            res.status(401).json({
+                error:"Invalid username or password"
+            })
+        }
+        const token = jwt.sign({_id:user._id,role:user.role},"key",{expiresIn:"1h"});
+        return res.status(200).json({
+            user:user,
+            access:token,
+            success:true
+        })
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({
+            error:"Internal server error"
+        })
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
