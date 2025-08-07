3a2fcf8a25657e16be57b50885802857b7c351ed
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-07 21:59:56 +0530

    practice

--- 19_node/src/routes/employeeRoutes.js
+++ 19_node/src/routes/employeeRoutes.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const {createEmployee,getAllEmployee,getEmployeeById,updatEmployee,deleteEmployee} = require('../controller/employeeController');
+const {auth,manager} = require('../middleware/auth');
 const router = express.Router();
 
-router.post("/",createEmployee);
-router.get("/",getAllEmployee);
-router.get("/:id",getEmployeeById);
-router.put("/:id",updatEmployee);
-router.delete("/:id",deleteEmployee);
+router.post(
+    "/",
+    auth,
+    manager,
+    createEmployee
+);
+router.get(
+    "/",
+    auth,
+    manager,
+    getAllEmployee
+);
+router.get(
+    "/:id",
+    auth,
+    getEmployeeById);
+router.put(
+    "/:id",
+    auth,
+    manager,
+    updatEmployee);
+router.delete(
+    "/:id",
+    auth,
+    manager,
+    deleteEmployee);
+    
 module.exports = router;
