e3154737d2531e426c4c66763ac0bfc00abf10b8
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-07 21:42:54 +0530

    practice

---
+++ 19_node/src/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+exports.auth = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({error:"Unauthorized, no token provided"});
+    }
+    const token = authHeader.split(" ")[1];
+    try{
+        const decoded = jwt.verify(token,"key");
+        req.user = decoded;
+        next();
+    }catch(err){
+        res.status(401).json({
+            error:"Unauthorized, invalid token"
+        })
+    }
+}
