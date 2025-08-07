c4e5b2b77de9449639a31afe673ed7705f804ee6
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-07 21:56:30 +0530

    practice

--- 19_node/src/middleware/auth.js
+++ 19_node/src/middleware/auth.js
@@ -17,3 +17,19 @@ exports.auth = (req,res,next)=>{
         })
     }
 }
+
+
+exports.manager = (req,res,next)=>{
+    if(req.user || !req.user.role){
+        return res.status(403).json({
+            msg:"Access Denied! No user role found."
+        })
+    }
+
+    if(req.user.role !== "manager"){
+        return res.status(403).json({
+            msg:"Access Denied! Managers only."
+        })
+    }
+    next();
+}
