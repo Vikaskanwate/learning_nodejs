3c80488a0679d49f550c52145b00ae2f5b470638
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-08 22:37:35 +0530

    practice

---
+++ 20_node/src/config/connection.js
@@ -0,0 +1,13 @@
+const mongoose  = require('mongoose');
+
+const connectDB = async () =>{
+    try{
+        const conn = await mongoose.connect('mongodb://localhost:27017/crud7')
+        console.log("mongodb connected...");
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
