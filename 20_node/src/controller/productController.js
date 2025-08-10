7b893aa59da16b3786384357b89ea078ef63f109
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-10 10:51:23 +0530

    practice

--- 20_node/src/controller/productController.js
+++ 20_node/src/controller/productController.js
@@ -1,3 +1,4 @@
+const product = require('../models/product');
 const productmodel = require('../models/product');
 
 const createProduct = async (req,res) =>{
@@ -54,8 +55,34 @@ const getProductById = async (req,res)=>{
     }
 }
 
+const updateProduct = async (req,res)=>{
+    const {id} = req.params;
+    try{
+        const {name,price,instock} = req.body;
+        const product  = await productmodel.findById(id);
+        if(!product){
+            return res.status(404).json({
+                msg:"Product not found"
+            })
+        }
+        product.name = name || product.name;
+        product.price = price || product.price;
+        product.instock = instock || product.instock;
+        await product.save();
+        return res.status(200).json({
+            product:product
+        })
+    }catch(err){
+        console.log(err);        
+        return res.status(500).json({
+            msg:"Internal server error"
+        })
+    }
+}
+
 module.exports = {
     createProduct,
     getAllProduct,
-    getProductById
+    getProductById,
+    updateProduct
 }
--- 20_node/src/routes/productRoutes.js
+++ 20_node/src/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {createProduct,getAllProduct,getProductById} = require('../controller/productController')
+const {createProduct,getAllProduct,getProductById,updateProduct} = require('../controller/productController')
 const router = express.Router();
 
 
 router.post("/",createProduct);
 router.get("/",getAllProduct);
 router.get("/:id",getProductById);
+router.put("/:id",updateProduct);
 module.exports  = router;
