87e2df3fc15217c16822ffb831058bf73e6357cd
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-12 01:15:44 +0530

    practice

--- 20_node/src/middleware/auth.js
+++ 20_node/src/middleware/auth.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-exports.auth = (req,res,next)=>{
+exports.auth  = (req,res,next) =>{
     const authHeader = req.headers.authorization;
-    
+
     if(!authHeader || !authHeader.startsWith("Bearer ")){
-        return res.status(401).json({error:"Unauthorized, no token provided"});
+        return res.status(401).json({
+            error:"Unauthorized, no token provided"
+        })
     }
     const token = authHeader.split(" ")[1];
     try{
@@ -18,17 +20,16 @@ exports.auth = (req,res,next)=>{
     }
 }
 
-
-exports.manager = (req,res,next)=>{
+exports.admin = (req,res,next) =>{
     if(!req.user || !req.user.role){
         return res.status(403).json({
             msg:"Access Denied! No user role found."
         })
     }
-
-    if(req.user.role !== "manager"){
+    if(req.user.role !== "Admin"){
         return res.status(403).json({
             msg:"Access Denied! Managers only."
+
         })
     }
     next();
