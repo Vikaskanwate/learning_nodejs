f38019b1789d19b8768a16cd4093ff9ba8d21174
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-14 09:25:02 +0530

    practice

---
+++ 21_node/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const connectDB = require("./config/connection");
+
+const app = express();
+
+connectDB();
+
+app.listen(3000,()=>{
+    console.log("Application is started");
+    
+});
