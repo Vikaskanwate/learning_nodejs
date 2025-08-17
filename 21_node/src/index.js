d8a39019d8c4ce1273614b2e39de4e2865678745
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-17 19:43:14 +0530

    practice

--- 21_node/src/index.js
+++ 21_node/src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/connection");
 const vehicalRouter = require('./routes/vehicalRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
 connectDB();
 
 app.use("",vehicalRouter);
+app.use("/user",userRouter);
 
 app.listen(3000,()=>{
     console.log("Application is started");
