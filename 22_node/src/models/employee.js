d42ed3820378765e04fc40b8b9fe1d200fffb454
Vikaskanwate <vikaskanwate32@gmail.com>
2025-09-06 23:29:30 +0530

    practice

--- 22_node/src/models/employee.js
+++ 22_node/src/models/employee.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
-const employeeShema = new mongoose.Schema({
+const employeeSchema = new mongoose.Schema({
     name:{
         type:String,
     },
     designation:{
         type:String
     },
+    department:{
+        type:String
+    },
+    salary:{
+        type:String
+    }
 })
+
+module.exports = mongoose.model("Employee",employeeSchema);
