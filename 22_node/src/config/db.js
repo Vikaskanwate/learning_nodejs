52df51cc5c2ac131358b8bda990a50da419e0f96
Vikaskanwate <vikaskanwate32@gmail.com>
2025-09-07 19:57:14 +0530

    practice

--- 22_node/src/config/db.js
+++ 22_node/src/config/db.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose')
 const connectDB = async ()=>{
     try{
         const conn = await mongoose.connect("mongodb://localhost:27017/crud9");
+        console.log("mongodb is connected on port",mongoose.connection.port);
+        
     }catch(err){
         console.log(err);
         process.exit(1);
     }
 }
+
+module.exports = connectDB;
--- 22_node/src/index.js
+++ 22_node/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const connectDb = require('../src/config/db');
 const app = express();
+
+connectDb();
 
 app.listen(3000,()=>{
     console.log("Application is started on port 3000");
