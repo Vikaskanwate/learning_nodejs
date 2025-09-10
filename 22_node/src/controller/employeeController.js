2603e91d104cd31e6fbd8f1bf378f07bb5275c9d
Vikaskanwate <vikaskanwate32@gmail.com>
2025-09-10 09:16:43 +0530

    practice

--- 22_node/src/controller/employeeController.js
+++ 22_node/src/controller/employeeController.js
@@ -1,37 +1,53 @@
 const employeemodel = require('../models/employee');
 const mongoose = require('mongoose');
 
-const createEmployee = async (req,res)=>{
-    const {name,designation,department,salary} = req.body;
-    try{
-
-    }catch(err){
+const createEmployee = async (req, res) => {
+    const { name, designation, department, salary } = req.body;
+    try {
+        const employee = await employeemodel.findOne({ name });
+        if (!employee) {
+            const newEmployee = new employeemodel({
+                name, designation, department, salary
+            })
+            await newEmployee.save();
+            return res.status(201).json({
+                employee: newEmployee,
+                success: true
+            })
+        }
+        return res.status(400).json({
+            msg: "Employee already exists",
+            success: false
+        })
+    } catch (err) {
         console.log(err);
         return res.status(500).json({
-            msg:"Internal server error"
+            msg: "Internal server error"
         })
     }
 }
 
-const getAllEmployee = async ()=>{
-    try{
+const getAllEmployee = async (req, res) => {
+    try {
         const employee = await employeemodel.find();
-        if(employee){
+        if (employee) {
             return res.status(200).json({
-                employee:employee,
-                success:true
+                employee: employee,
+                success: true
             })
         }
         return res.status(404).json({
-            msg:"Employee not found"
+            msg: "Employee not found"
         })
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return res.status(500).json({
-            msg:"Internal server error"
+            msg: "Internal server error"
         })
     }
 }
+
+const getEmployeeById = async(req, res)
 
 
 module.exports = {
