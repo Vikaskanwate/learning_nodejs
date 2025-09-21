aa8ae3f2c59e0a8379fd2eacddf4b6559c16c207
Vikaskanwate <vikaskanwate32@gmail.com>
2025-09-21 20:18:39 +0530

    practice

--- 20_node/src/index.js
+++ 20_node/src/index.js
@@ -3,11 +3,13 @@ const connectDB = require('./config/connection');
 const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
+const cors = require('cors')
 app.use(express.json());
+app.use(cors());
 
 connectDB();
 
-app.use("",productRouter);
+app.use("/api",productRouter);
 app.use("/user",userRouter);
 
 app.listen(3000,()=>{
